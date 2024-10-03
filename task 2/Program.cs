dfca4c9ccfc6aac829332b0144c6052e22881fa3
nataliejapharidze <natalie.japharidze@mziuri.ge>
2024-10-04 00:15:46 +0400

    Fix Task 2 issues: 1. Fix quality issues 2. FIx Folder Name

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -11,16 +11,17 @@ new Profile(3,"jean","u",new DateTime(1994,11,1)),
 new Profile(4,"carl","a",new DateTime(2004,4,23)),
 };
 
-    
-app.MapGet("/profiles", () => { return TypedResults.Ok(profiles); });
+app.MapGet("/profiles", () =>
+{
+    return TypedResults.Ok(profiles);
+});
 
-app.MapGet("/profile/{id}", Ok<Profile>(int id) => {
+app.MapGet("/profile/{id}", Ok<Profile> (int id) =>
+{
     var prof = profiles.SingleOrDefault(x => x.id == id);
     return TypedResults.Ok(prof);
 });
 
-
 app.Run();
 
-
-public record Profile(int id, string name, string description,DateTime dateofbirth);
+public record Profile(int id, string name, string description, DateTime dateofbirth);
