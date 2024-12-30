961ffbbaaba93f7dc675d19da337e4774df72bef
andria abashidze <andriaaqaa@gmail.com>
2024-12-30 15:04:48 +0400

    Practice 4: added 14-20

--- Practice 4/Query.sql
+++ Practice 4/Query.sql
@@ -2,7 +2,7 @@
 SELECT model, speed, hd FROM PC WHERE price<500
 
 --2
-SELECT DISTINCT maker FROM Product WHERE type ='printer' 
+SELECT maker FROM Product WHERE type ='printer' 
 
 --3
 SELECT model, ram, screen FROM laptop WHERE price>1000 
@@ -43,3 +43,36 @@ SELECT AVG(speed) FROM Laptop WHERE price>1000
 
 --13
 SELECT AVG(speed) FROM Product JOIN PC ON (Product.model = PC.model) WHERE maker ='A' 
+
+--14
+SELECT C.class, name, country FROM Classes C JOIN Ships S ON S.class=C.class WHERE C.numGuns>=10
+
+--15
+SELECT hd
+FROM PC
+GROUP BY hd
+HAVING COUNT(hd)>=2
+
+--16
+select DISTINCT p1.model, p2.model, p1.speed, p2.ram
+FROM PC p1, PC p2
+WHERE p1.speed = p2.speed AND p1.ram = p2.ram AND  p1.model != p2.model AND p1.model >p2.model
+
+--17
+SELECT DISTINCT type, p.model, L.speed
+FROM Laptop L JOIN Product P ON (l.model=p.model) 
+WHERE speed < (SELECT MIN(speed) from PC)
+
+--18
+SELECT DISTINCT maker, price
+FROM Printer Pr JOIN Product Pro ON (Pr.model=Pro.model)
+WHERE Pr.price = (SELECT MIN(price) FROM Printer WHERE color = 'y')  AND Pr.color = 'y'
+
+--19
+SELECT maker, AVG(screen)
+FROM Laptop L JOIN Product P ON (L.model = P.model)
+GROUP BY maker
+
+--20
+SELECT P.maker, COUNT(DISTINCT P.model) FROM Product P WHERE type = 'PC'
+GROUP BY P.maker HAVING COUNT (DISTINCT P.model) >=3
